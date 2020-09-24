f29821c6b73fbf5a9944d1d43ecb18e5972de30e
anjelrana <anjelworkyt@gmail.com>
2020-09-24 22:25:29 +0530

    adding the templating

--- app.js
+++ app.js
@@ -4,19 +4,50 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res) {
-   
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
 
-    if (currentDay === 0) {
-        res.send("its a week end ");
-    } else {
-        
-        res.sendFile(__dirname +"/index.html");
+    
+
+    switch (currentDay) {
+        case 0:
+            day = "sunday";
+            break;
+        case 1:
+            day = "monday";
+            break;
+        case 2:
+            day = "tuesday";
+            break;
+        case 3:
+            day = "wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "friday";
+            break;
+        case 6:
+            day ="saturday";
+            break;
+
+        default: console.log("Error: the current day no. is " + currentDay );
+            break;
     }
+
+     
+    res.render("list", {
+        kindOfDay: day
+    });
+
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("The Server has started and working at the port 3000");
 });
