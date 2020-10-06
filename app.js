e6311e821a0f87ec21752626f605d03acff379e8
anjelrana <anjelworkyt@gmail.com>
2020-10-07 00:05:24 +0530

    data passing from web page to server and back and forth

--- app.js
+++ app.js
@@ -1,53 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
+let items =["code 7+ hours","clean room", "meditate"];
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine", "ejs");
 
+
+
 app.get("/", function (req, res) {
+let today = new Date();
 
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
 
-    
+};
 
-    switch (currentDay) {
-        case 0:
-            day = "sunday";
-            break;
-        case 1:
-            day = "monday";
-            break;
-        case 2:
-            day = "tuesday";
-            break;
-        case 3:
-            day = "wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "friday";
-            break;
-        case 6:
-            day ="saturday";
-            break;
+let day = today.toLocaleTimeString("en-IN", options);
 
-        default: console.log("Error: the current day no. is " + currentDay );
-            break;
-    }
-
-     
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day, newListItems: items
     });
 
+});
+app.post("/", function(req, res) {
+     var item = req.body.items;
+     items.push(item);
+     res.redirect("/");
 });
 
 app.listen(3000, function () {
     console.log("The Server has started and working at the port 3000");
 });
+
